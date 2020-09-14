20a74101840426ccce28a188d2072597236dbc52
Rafael Henrique <contato@umaguin.com>
2020-09-13 23:19:51 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -9,18 +9,5 @@ button.addEventListener("click", function () {
 
     answer.innerHTML= Math.floor(Math.random() * (max - min + 1) + min);
 
-    const layout = `
-    <h2>Aqui está o resultado:</h2>
-    <div class="result-content">
-      <ul>
-        <li>
-        ${answer}
-        </li>
-      </ul>
-    </div>
-  `;
-
-    const result = document.getElementById("result");
-
-    result.innerHTML = layout;
+   
 });
