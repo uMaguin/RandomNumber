2cc8c7b6a56036500f2eb38e9a45747da793f4c3
Rafael Henrique <contato@umaguin.com>
2020-09-22 21:45:02 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-    const max = parseInt(document.getElementById("max").value);
+  const max = parseInt(document.getElementById("max").value);
 
-    const min = parseInt(document.getElementById("min").value);
+  const min = parseInt(document.getElementById("min").value);
 
-    const answer = document.getElementById("result");
+  const answer = document.getElementById("result");
 
-    answer.innerHTML = Math.floor(Math.random() * (max - min + 1) + min);
+  answer.innerHTML = Math.floor(Math.random() * (max - min + 1) + min);
 });
